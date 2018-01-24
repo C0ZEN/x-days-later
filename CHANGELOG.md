4ab4704bc0ad2048b6824862ee109a8721b7d1b0
Testelin Geoffrey <geoffrey.testelin@gmail.com>
2018-01-24 22:15:49 +0100

    Release patch 0.16.32

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+## [0.16.32]
+### Changed
+- SW
+
 ## [0.16.31]
 ### Changed
 - SW
--- docs/release/x-days-later.js
+++ docs/release/x-days-later.js
@@ -726,7 +726,7 @@ function safeApply(scope, fn) {
 	config.$inject = [];
 
 	function config() {
-		console.info('Current version: 0.16.31');
+		console.info('Current version: 0.16.32');
 	}
 })(window.angular);
 
