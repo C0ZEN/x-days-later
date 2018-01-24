47ff1f9ef2fc1a0cbce31bdbe330bc6031575ec6
Testelin Geoffrey <geoffrey.testelin@gmail.com>
2018-01-24 19:51:27 +0100

    Release patch 0.16.19

--- docs/release/x-days-later.js
+++ docs/release/x-days-later.js
@@ -726,7 +726,7 @@ function safeApply(scope, fn) {
 	config.$inject = [];
 
 	function config() {
-		console.info('Current version: 0.16.18');
+		console.info('Current version: 0.16.19');
 	}
 })(window.angular);
 
