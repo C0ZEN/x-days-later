a8df15e96dcfa8001db1182f9233cac8874089db
Testelin Geoffrey <geoffrey.testelin@gmail.com>
2018-04-07 22:41:56 +0200

    Release patch 0.19.2

--- docs/release/x-days-later.js
+++ docs/release/x-days-later.js
@@ -728,7 +728,7 @@ function safeApply(scope, fn) {
 	config.$inject = [];
 
 	function config() {
-		console.info('Current version: 0.19.1');
+		console.info('Current version: 0.19.2');
 	}
 })(window.angular);
 
